4789030bc8405c029fa9f6a9aa06ee4eed380e2c
Faris Edrik P <farisedrik21@gmail.com>
2025-08-14 19:34:02 +0700

    add project files

---
+++ src/data.js
@@ -0,0 +1,234 @@
+import HeroImage from "/assets/hero-img.webp";
+
+const Image = {
+  HeroImage,
+};
+
+export default Image;
+
+import Tools1 from "/assets/tools/vscode.png";
+import Tools2 from "/assets/tools/reactjs.png";
+import Tools3 from "/assets/tools/nextjs.png";
+import Tools4 from "/assets/tools/tailwind.png";
+import Tools5 from "/assets/tools/bootstrap.png";
+import Tools6 from "/assets/tools/js.png";
+import Tools7 from "/assets/tools/nodejs.png";
+import Tools8 from "/assets/tools/github.png";
+import Tools9 from "/assets/tools/ai.png";
+import Tools10 from "/assets/tools/canva.png";
+import Tools11 from "/assets/tools/figma.png";
+import Tools12 from "/assets/tools/kotlin.png";
+import Tools13 from "/assets/tools/firebase.png";
+import Tools14 from "/assets/tools/html.png";
+import Tools15 from "/assets/tools/css.png";
+import Tools16 from "/assets/tools/ts.png";
+import Tools17 from "/assets/tools/php.png";
+import Tools18 from "/assets/tools/vite.png";
+import Tools19 from "/assets/tools/mysql.png";
+
+
+export const listTools = [
+  {
+    id: 1,
+    gambar: Tools1,
+    nama: "Visual Studio Code",
+    ket: "Code Editor",
+    dad: "100",
+  },
+  {
+    id: 2,
+    gambar: Tools2,
+    nama: "React JS",
+    ket: "Framework",
+    dad: "200",
+  },
+  {
+    id: 3,
+    gambar: Tools3,
+    nama: "Next JS",
+    ket: "Framework",
+    dad: "300",
+  },
+  {
+    id: 4,
+    gambar: Tools4,
+    nama: "Tailwind CSS",
+    ket: "Framework",
+    dad: "400",
+  },
+  {
+    id: 5,
+    gambar: Tools5,
+    nama: "Bootstrap",
+    ket: "Framework",
+    dad: "500",
+  },
+  {
+    id: 6,
+    gambar: Tools6,
+    nama: "Javascript",
+    ket: "Language",
+    dad: "600",
+  },
+  {
+    id: 7,
+    gambar: Tools7,
+    nama: "Node JS",
+    ket: "Javascript Runtime",
+    dad: "700",
+  },
+  {
+    id: 8,
+    gambar: Tools8,
+    nama: "Github",
+    ket: "Repository",
+    dad: "800",
+  },
+  {
+    id: 9,
+    gambar: Tools9,
+    nama: "Adobe Illustrator",
+    ket: "Design App",
+    dad: "900",
+  },
+  {
+    id: 10,
+    gambar: Tools10,
+    nama: "Canva",
+    ket: "Design App",
+    dad: "1000",
+  },
+  {
+    id: 11,
+    gambar: Tools11,
+    nama: "Figma",
+    ket: "Design App",
+    dad: "1100",
+  },
+  {
+    id: 12,
+    gambar: Tools12,
+    nama: "Kotlin",
+    ket: "Language",
+    dad: "1200",
+  },
+  {
+    id: 13,
+    gambar: Tools13,
+    nama: "Firebase",
+    ket: "Framework",
+    dad: "1300",
+  },
+  {
+    id: 14,
+    gambar: Tools14,
+    nama: "HTML",
+    ket: "Language",
+    dad: "1400",
+  },
+  {
+    id: 15,
+    gambar: Tools15,
+    nama: "CSS",
+    ket: "Language",
+    dad: "1500",
+  },
+  {
+    id: 16,
+    gambar: Tools16,
+    nama: "TypeScript",
+    ket: "Language",
+    dad: "1600",
+  },
+  {
+    id: 17,
+    gambar: Tools17,
+    nama: "PHP",
+    ket: "Language",
+    dad: "1700",
+  },
+  {
+    id: 18,
+    gambar: Tools18,
+    nama: "Vite",
+    ket: "Framework",
+    dad: "1800",
+  },
+  {
+    id: 19,
+    gambar: Tools19,
+    nama: "MySql",
+    ket: "Framework",
+    dad: "1900",
+  },
+];
+
+import Proyek1 from "/assets/proyek/proyek1.webp";
+import Proyek2 from "/assets/proyek/proyek2.webp";
+import Proyek3 from "/assets/proyek/proyek3.webp";
+import Proyek4 from "/assets/proyek/proyek4.webp";
+import Proyek5 from "/assets/proyek/proyek5.webp";
+import Proyek6 from "/assets/proyek/proyek6.webp";
+
+export const listProyek = [
+  {
+    id: 1,
+    image: Proyek1,
+    title: "Website Sekolah",
+    subtitle: "HTML CSS Javascript AOS",
+    borderColor: "#3B82F6",
+    gradient: "linear-gradient(145deg, #3B82F6, #000)",
+    url: "https://github.com/sarahjohnson",
+    dad: "100",
+  },
+  {
+    id: 2,
+    image: Proyek2,
+    title: "Company Profile",
+    subtitle: "HTML CSS Javascript AOS Swiper Lightbox Gallery",
+    borderColor: "#10B981",
+    gradient: "linear-gradient(180deg, #10B981, #000)",
+    url: "https://linkedin.com/in/mikechen",
+    dad: "200",
+  },
+  {
+    id: 3,
+    image: Proyek3,
+    title: "Web Pernikahan 2.0",
+    subtitle: "Vite ReactJS TailwindCSS AOS",
+    borderColor: "#3B82F6",
+    gradient: "linear-gradient(145deg, #3B82F6, #000)",
+    url: "https://github.com/sarahjohnson",
+    dad: "300",
+  },
+  {
+    id: 4,
+    image: Proyek4,
+    title: "Website Course",
+    subtitle: "Vite ReactJS Bootstrap AOS",
+    borderColor: "#10B981",
+    gradient: "linear-gradient(180deg, #10B981, #000)",
+    url: "https://linkedin.com/in/mikechen",
+    dad: "400",
+  },
+    {
+    id: 5,
+    image: Proyek5,
+    title: "Web Portfolio",
+    subtitle: "HTML CSS Javascript Bootsrap",
+    borderColor: "#3B82F6",
+    gradient: "linear-gradient(145deg, #3B82F6, #000)",
+    url: "https://github.com/sarahjohnson",
+    dad: "500",
+  },
+  {
+    id: 6,
+    image: Proyek6,
+    title: "Company Profile 2.0",
+    subtitle: "NextJS TailwindCSS Framermotion",
+    borderColor: "#10B981",
+    gradient: "linear-gradient(180deg, #10B981, #000)",
+    url: "https://linkedin.com/in/mikechen",
+    dad: "600",
+  },
+];
