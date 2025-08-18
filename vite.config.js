4bfa4316d4c2dabc69a5f46709e207098679128f
BangAldy21 <anggachannel011@gmail.com>
2025-08-18 19:19:49 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portofolio/",
+  base: "/Aldy02211/",
   plugins: [react(), tailwindcss()],
 })
