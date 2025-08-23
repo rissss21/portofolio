4f799f3f05c13e6e6962b95e7f4f20278696bbfc
BangAldy21 <anggachannel011@gmail.com>
2025-08-24 01:41:56 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Navbar from './src/Navbar.jsx'
-import Footer from './src/Footer.jsx'
-import PreLoader from './src/PreLoader.jsx'
+import Navbar from './Navbar.jsx'
+import Footer from './Footer.jsx'
+import PreLoader from './PreLoader.jsx'
 import "animate.css"
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
